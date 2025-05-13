1ec199cf7f68a2919d5bd33be49b5e0a53a94fd5
Hassane <hassanedjibrinasabou@gmail.com>
2025-05-14 00:10:36 +0200

    Mon premier depot

---
+++ Exo 1/hello_world.sh
@@ -0,0 +1,7 @@
+# creation du fichier source
+echo '#include <stdio.h> #importation de la bibliotheque stdio
+# fonction principal
+int main(){
+printf("Hello World"); # fonction d'affichage
+return 0;
+}'  > hello_world.c
