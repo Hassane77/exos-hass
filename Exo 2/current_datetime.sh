63e1045879424e0a50f59aa9f4fadad35732a5df
Hassane77 <hassanedjibrinasabou@gmail.com>
2025-05-17 15:08:45 +0200

    Update current_datetime.sh

--- Exo 2/current_datetime.sh
+++ Exo 2/current_datetime.sh
@@ -1,12 +1,7 @@
 # creation du fichier source
-echo '#include <stdio.h> #importation de la bibliotheque stdio
-#include <time.h>
-# fonction principal
-int man(){
+echo"
     time_t t = time(null); #permet d'optenir l'heure actuelle
     strict tm *tm_info = localtime(&t); #permet de convertir en heure local
     buffer[20]; #permet de stocker la date formate
     strtime(buffer,sizeof(buffer),"%y-%m-%d %H:%M:%S"); #permet de formate ladate au bon format
-    print("date actuelle : %s",buffer); #permet d'afficher la date
-    return 0;
-}'
+    print("date actuelle : %s",buffer);" #permet d'afficher la date
